7d140ac1cca72ac2b9c4bded06ed265f3063916d
codeme254 <otwomadennis365@gmail.com>
2022-11-05 12:10:46 +0300

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+/**
+ *bubble_sort - sorts an array using the bubble sort algorithm
+ *@array: the array to be sorted
+ *@size: size of the array to be sorted
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t j;
+	bool swapped;
+
+	for (i = 0; i < size; i++)
+	{
+		swapped = false;
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				int tmp = array[j];
+
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				print_array(array, size);
+				swapped = true;
+			}
+		}
+		if (swapped == false)
+		{
+			break;
+		}
+	}
+}
+
