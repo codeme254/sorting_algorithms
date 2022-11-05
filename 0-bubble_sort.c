26a64b6aa08591ee020b733ad6b9a6bf43541d49
codeme254 <otwomadennis365@gmail.com>
2022-11-05 12:39:14 +0300

    fixing req 1

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,18 +1,22 @@
 #include "sort.h"
+
 /**
  *bubble_sort - sorts an array using the bubble sort algorithm
  *@array: the array to be sorted
  *@size: size of the array to be sorted
 */
+
 void bubble_sort(int *array, size_t size)
 {
 	size_t i;
 	size_t j;
+
 	/*bool swapped;*/
 
 	for (i = 0; i < size; i++)
 	{
 		/*swapped = false;*/
+
 		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
@@ -22,13 +26,11 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
 				print_array(array, size);
+
 				/*swapped = true;*/
 			}
 		}
-		/*if (swapped == false)
-		{
-			break;
-		}*/
+
 	}
 }
 
